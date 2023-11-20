f8006f13b587a34d4c4651dcc784e3635236bc3b
DESKTOP-VVN7SIU\User <nunnapatppp@gmail.com>
2023-11-20 23:05:38 +0700

    up to date

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./components/nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hi</h1>
-        <h2>hello</h2>
-        <h3>hii</h3>
-        <h4>hello again for test commit</h4>
-      </header>
-    </div>
+    <>
+      <Nav />
+    </>
   );
 }
 
---
+++ frontend/src/components/nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function nav() {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Blog</a>
+      <a href="#">Contact</a>
+    </div>
+  );
+}
+
+export default nav;
